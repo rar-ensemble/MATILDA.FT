239504d5ab7564ebd8f2450de4746e7f390a07c2
Rob Riggleman <rrig@seas.upenn.edu>
2024-11-02 13:36:56 -0400

    cleaning up utils folder in prep for adding new features

--- utils/extract-resume.sh
+++ utils/extract-resume.sh
@@ -1,9 +1,16 @@
 #!/bin/sh
 # bash commands to extract the final frame from traj.lammpstrj into
 # resume.lammpstrj for the read_resume command. 
-# This routine WILL OVERWRITE any existing resume.lammpstrj
+# This routine WILL OVERWRITE any existing output file
+# if arguments given, then the read filename is the first one
+# and the output filename is the second argument
 
-ns=`head -n 4 traj.lammpstrj | tail -n 1`
-nlines=$(($ns+9))
-
-tail -n ${nlines} traj.lammpstrj >resume.lammpstrj
+if [ "$#" -ne 2 ]; then
+    ns=`head -n 4 traj.lammpstrj | tail -n 1`
+    nlines=$(($ns+9))
+    tail -n ${nlines} traj.lammpstrj >resume.lammpstrj
+  else
+    ns=`head -n 4 $1 | tail -n 1`
+    nlines=$(($ns+9))
+    tail -n ${nlines} $1 > $2
+fi
