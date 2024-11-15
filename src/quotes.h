1fa832a4779375778fb53b6dffb7c20f4a2abfac
Rob Riggleman <rrig@seas.upenn.edu>
2024-11-15 14:55:39 -0500

    added more quotes to the list

--- src/quotes.h
+++ src/quotes.h
@@ -23,6 +23,19 @@ std::vector<std::string> quoteDB = {
   "'He wakes, crusted in vomit' \n--GWAR",
   "'I'll put a gun to a bunny like choose, say something funny or bunny go boom' \n--Run the Jewels",
   "'I will eat your soul' \n--Aphex Twin",
+  "'Triangle man, triangle man, triangle man hates person man; they have a fight, triangle wins.' \n--They Might Be Giants",
+  "'Bite my shiny metal ass' \n--Bender",
+  "'Libertarians are like house cats: convinced of their fierce independence while dependent on a system they neither appreciate nor understand' \n--Unknown",
+  "'I'm against protesting, but I don't know how to show it' \n--Mitch Hedberg",
+  "'Think about how stupid the average person is...and then realize that half of them are stupider than that!' \n--George Carlin",
+  "'When a friend asks for help, ya help 'em' \n--Letterkenny",
+  "'Well first of all, I'd have to be a good person to remain one...' \n--Drew Morgan",
+  "'If you stands for nothing, you'll fall for anythings.' \n--Squirrely Dan",
+  "'Its chaos; be kind.' \n--Patton Oswalt",
+  "'Trying hrder is not the solution if effort is not the problem.' \n--How to ADHD",
+  "'If you're in heaven then you'll forgive me, dear, cause that's what they do up there.' \n--Nick Cave and the Bad Seeds",
+  "'How do flat earthers travel??\n\n........\n\nOn a plane.' \n--DockTok",
+  "'I was sitting in traffic the other day.\n\n..........\n\nIt's probably why I got run over.' \n--DockTok",
   "'Would I be a good messiah with my low self-esteem? If I don't believe in myself would that be blasphemy?' \n--Bloodhound Gang" };
 
 #endif
