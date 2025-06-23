74384e8e6defe01c973dfa09120c7ace92bdf11e
Rob Riggleman <rrig@seas.upenn.edu>
2025-06-23 15:08:32 -0400

    Found minor bug that affects DMGrid value when nstot < M

--- src/PS_Box_initialization.cu
+++ src/PS_Box_initialization.cu
@@ -341,7 +341,7 @@ void PS_Box::finishInitialization() {
     // gpuGrid, block sizes
     M_Block = blockSize;
     M_Grid = (int)ceil((double)(M) / M_Block);
-    DMGrid = (int)ceil((double)(Dim*nstot) / M_Block);
+    DMGrid = (int)ceil((double)(Dim*M) / M_Block);
 
     nsBlock = blockSize;
     nsGrid = (int)ceil((double)(nstot) / nsBlock);
